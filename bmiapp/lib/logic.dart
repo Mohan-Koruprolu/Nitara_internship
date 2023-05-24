b389ed6b52a3675db1f9ae5d1473979152c19cc4
MOHAN_KORUPROLU <mohankoruprolu01@gmail.com>
2023-05-24 16:38:02 +0530

    Response react based on BMI value only

--- bmiapp/lib/logic.dart
+++ bmiapp/lib/logic.dart
@@ -42,8 +42,6 @@ class RequestModel {
 
 class ResponseModel {
   double _bmi = 0.0;
-  double _height = 0.0;
-  double _weight = 0.0;
   List<String> img = [
     "https://th.bing.com/th/id/OIP.XJOSqzpR817O9ZPXqgtt2gHafl?w=82&h=349&c=7&r=0&o=5&dpr=1.3&pid=1.7",
     "https://th.bing.com/th/id/OIP.MaCmnZkgicn7CjbT297tbwHaPR?w=115&h=197&c=7&r=0&o=5&dpr=1.3&pid=1.7",
