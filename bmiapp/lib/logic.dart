d415fb386b82e547dd619047064b800f92a7c2ea
MOHAN_KORUPROLU <mohankoruprolu01@gmail.com>
2023-05-24 16:31:49 +0530

    response reacts based on bmi only

--- bmiapp/lib/logic.dart
+++ bmiapp/lib/logic.dart
@@ -9,11 +9,11 @@ class Logic {
   }
   Logic(double height, double weight) {
     a = RequestModel(height, weight);
-    b = ResponseModel(a?.height, a?.weight);
+    b = ResponseModel(getbmi()!);
   }
 
   double? getbmi() {
-    return b?.Bmi;
+    return a?.bmi;
   }
 
   String? getimg() {
@@ -24,7 +24,9 @@ class Logic {
 class RequestModel {
   double _height = 0.0;
   double _weight = 0.0;
-  RequestModel(this._height, this._weight);
+  double _bmi = 0.0;
+  RequestModel(this._height, this._weight)
+      : _bmi = _weight / pow(_height / 100, 2);
   get height {
     return _height;
   }
@@ -32,44 +34,33 @@ class RequestModel {
   get weight {
     return _weight;
   }
+
+  get bmi {
+    return _bmi;
+  }
 }
 
 class ResponseModel {
   double _bmi = 0.0;
   double _height = 0.0;
   double _weight = 0.0;
-  double bmi = 0;
   List<String> img = [
     "https://th.bing.com/th/id/OIP.XJOSqzpR817O9ZPXqgtt2gHafl?w=82&h=349&c=7&r=0&o=5&dpr=1.3&pid=1.7",
     "https://th.bing.com/th/id/OIP.MaCmnZkgicn7CjbT297tbwHaPR?w=115&h=197&c=7&r=0&o=5&dpr=1.3&pid=1.7",
     'https://th.bing.com/th/id/OIP.CwWKuANPjNhg2JapdA89VgHaHa?w=194&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7'
   ];
   int _level = 0;
-  ResponseModel(this._height, this._weight) {
-    if (_height > 0 && _weight > 0) {
-      _bmi = _weight / pow(_height / 100, 2);
-      _level = getlevel(bmi);
+  ResponseModel(double bmi) {
+    _bmi = bmi;
+    if (_bmi < 18.5) {
+      _level = 0;
+    } else if (_bmi >= 18.5 && _bmi < 25) {
+      _level = 1;
     } else {
-      _bmi = 0;
-      _level = getlevel(bmi);
+      _level = 2;
     }
   }
-
-  double? get Bmi {
-    return _bmi;
-  }
-
   String getimg() {
     return img[_level];
-  }
-
-  int getlevel(double bmi) {
-    if (_bmi < 18.5) {
-      return _level = 0;
-    } else if (_bmi >= 18.5 && _bmi < 25) {
-      return _level = 1;
-    } else {
-      return _level = 2;
-    }
   }
 }
